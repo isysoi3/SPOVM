6c22710c0b5a80c5ea6102493e5dd94464e6ebc4
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-04-04 08:47:53 +0300

    added empty initial files

---
+++ lab2/linux/parent.cpp
@@ -0,0 +1,15 @@
+#include <ctime>    
+#include <unistd.h>
+#include <ncurses.h>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <cstdlib>
+
+int main() {
+
+    return 0;
+}
