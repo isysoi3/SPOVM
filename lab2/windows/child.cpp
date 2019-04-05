e61b90e467d30e2a986cf600269641a43576419d
liza <123yak12@gmail.com>
2019-04-05 21:35:48 +0300

    lab2[w]: completely add task

--- lab2/windows/child.cpp
+++ lab2/windows/child.cpp
@@ -2,10 +2,26 @@
 #include <windows.h>
 #define DELAY_TIME_SEC 2
 
-int main() {
-    while (true) {
-        std::cout << "Hello world!" << std::endl;
+int main(int argc, char *argv[])
+{
+    HANDLE hEndEvent = OpenEvent(SYNCHRONIZE, FALSE, argv[1]);
+    if (hEndEvent == NULL)
+    {
+        std::cout << "Open event failed.\n";
+        return GetLastError();
+    }
+
+    while (true)
+    {
+        DWORD dwWaitResult = WaitForSingleObject(hEndEvent, 0);
+        if (dwWaitResult == WAIT_OBJECT_0)
+        {
+            break;
+        }
+        std::cout << "Child process string: " << argv[1] << '\n';
         Sleep(DELAY_TIME_SEC * 1000);
     }
+
+    CloseHandle(hEndEvent);
     return 0;
 }
--- lab2/windows/parent.cpp
+++ lab2/windows/parent.cpp
@@ -2,54 +2,112 @@
 #include <stack>
 #include <windows.h>
 
+#define CHILD_PROCESS_NAME "child.exe"
+#define BUF_SIZE 256
 
-std::stack <HANDLE> childHandles; 
+std::stack<std::pair<PROCESS_INFORMATION, HANDLE>> childProcesses;
 
-void printMenu() {
-    std::cout << "Child processes: " << childHandles.size() << std::endl;
+int newProcessNum = 0;
+
+void printMenu()
+{
+    std::cout << "Child processes: " << childProcesses.size() << "\n";
     std::cout << "[+] - add process\n";
     std::cout << "[-] - remove process\n";
     std::cout << "[q] - exit\n";
 }
 
-void addChildProcess() {
-    std::cout << "Add new child process will be here\n";
+HANDLE createEvent()
+{
+    char endEventName[BUF_SIZE] = "";
+    sprintf(endEventName, "%d", newProcessNum);
+    HANDLE handleEndEvent = CreateEvent(NULL, FALSE, FALSE, endEventName);
+    if (handleEndEvent == NULL)
+    {
+        std::cout << "Failed to create end event for new process";
+    }
+    return handleEndEvent;
 }
 
-void removeChildProcess() {
-     std::cout << "Remove child process will be here\n";
+void addChildProcess()
+{
+    HANDLE handleEndEvent = createEvent();
+    if (handleEndEvent == NULL)
+    {
+        return;
+    }
+    char childProcessArgs[BUF_SIZE] = "";
+    sprintf(childProcessArgs, "%s %d", CHILD_PROCESS_NAME, newProcessNum);
+    STARTUPINFO si;
+    PROCESS_INFORMATION piApp;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    if (!CreateProcess(NULL, childProcessArgs, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &piApp))
+    {
+        std::cout << "Failed to create new process\n";
+        return;
+    }
+    newProcessNum++;
+    childProcesses.push(std::make_pair(piApp, handleEndEvent));
 }
 
-void removeAllChildProcess() {
-    std::cout << "Add all child processes will be here\n";
-    while(!childHandles.empty()) {
-        // pid_t childPid = childPids.top();
-        // childPids.pop();
-        // kill(childPid, SIGKILL);
+void killProcess(std::pair<PROCESS_INFORMATION, HANDLE> childProcess)
+{
+    SetEvent(childProcess.second);
+    WaitForSingleObject(childProcess.first.hProcess, INFINITE);
+    CloseHandle(childProcess.second);
+    CloseHandle(childProcess.first.hThread);
+    CloseHandle(childProcess.first.hProcess);
+}
+
+void removeChildProcess()
+{
+    if (childProcesses.empty())
+    {
+        std::cout << "Nothing to delete.\n";
+    }
+    else
+    {
+        std::pair<PROCESS_INFORMATION, HANDLE> childProcess = childProcesses.top();
+        childProcesses.pop();
+        killProcess(childProcess);
     }
 }
 
-int main() {
+void removeAllChildProcess()
+{
+    while (!childProcesses.empty())
+    {
+        std::pair<PROCESS_INFORMATION, HANDLE> childProcess = childProcesses.top();
+        childProcesses.pop();
+        killProcess(childProcess);
+    }
+}
+
+int main()
+{
     char operation;
     bool isFinished = false;
 
-    while(!isFinished) {
+    while (!isFinished)
+    {
         printMenu();
         std::cin >> operation;
         std::cin.ignore();
-        switch (operation) {
-            case '+':
-                addChildProcess();
-                break;
-            case '-':
-                removeChildProcess();
-                break;
-            case 'q':
-                removeAllChildProcess();
-                isFinished = true;
-                break;
-            default:
-                break;
+        switch (operation)
+        {
+        case '+':
+            addChildProcess();
+            break;
+        case '-':
+            removeChildProcess();
+            break;
+        case 'q':
+            removeAllChildProcess();
+            isFinished = true;
+            break;
+        default:
+            break;
         }
     }
 
