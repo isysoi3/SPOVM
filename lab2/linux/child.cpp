41174a12643aa6032117bd100841ca92c416d61b
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-04-24 16:16:30 +0300

    lab2[l]: updated string printing

--- lab2/linux/child.cpp
+++ lab2/linux/child.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <unistd.h>
+#include <string>
+#include <signal.h>
 
 #define DELAY_TIME_SEC 2
 
 int main() {
+    std::string processString = "Process id - " + std::to_string(getpid());
     while (true) {
-        std::cout << "Process id - " << getpid() << std::endl;
+        for(std::string::size_type i = 0; i < processString.size(); ++i) {
+            std::cout << processString[i];
+        } 
+        std::cout << std::endl;
         sleep(DELAY_TIME_SEC);
     }
     return 0;
