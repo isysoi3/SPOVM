d60a60d0be6b80bca1eb18d17dde8fc905fe4a64
liza <123yak12@gmail.com>
2019-04-28 20:52:57 +0300

    lab3[w]: added empty files for 3 lab

---
+++ lab3/windows/client.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Windows.h>
+
+
+int main() {
+    std::cout << "Hello, i'm client!";
+    return 0;
+}
---
+++ lab3/windows/server.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Windows.h>
+
+
+int main() {
+    std::cout << "Hello, i'm server!";
+    return 0;
+}
