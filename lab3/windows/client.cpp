dbbfc98d40520840a753d66256688f8bd70eb97c
liza <123yak12@gmail.com>
2019-04-28 22:31:06 +0300

    lab3[w]: completely add programm logic

--- lab3/windows/client.cpp
+++ lab3/windows/client.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <Windows.h>
+#include <string.h>
+
+#define CLIENT_END_EVENT_NAME "CLIENT_END_EVENT"
+#define PIPE_NAME "\\\\.\\pipe\\server_pipe"
+
+#define LINE_LEN 512
+
+int main()
+{
+    HANDLE clientEndEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, CLIENT_END_EVENT_NAME);
+    if (clientEndEvent == NULL)
+    {
+        std::cout << "Opening client end event failed." << '\n';
+        return GetLastError();
+    }
+
+    HANDLE pipe = CreateFile(PIPE_NAME, GENERIC_READ, FILE_SHARE_READ,NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    
+    if(pipe == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Opening pipe failed." << '\n';
+        CloseHandle(clientEndEvent);
+        return GetLastError();
+    }
 
 
-int main() {
-    std::cout << "Hello, i'm client!";
+    char str [LINE_LEN];
+    DWORD dwBytesRead;
+    while(true){
+        if (!ReadFile(pipe,  str, sizeof(str), &dwBytesRead, NULL))  {   
+            CloseHandle(pipe);   
+            return GetLastError();  
+        }
+        std::cout << str << '\n';
+        SetEvent(clientEndEvent);
+        if (strcmp(str, "q") == 0)
+        {
+            break;
+        }
+    }
+    
+    std::cout << "Client process finish work.";
+    CloseHandle(clientEndEvent);
     return 0;
 }
--- lab3/windows/server.cpp
+++ lab3/windows/server.cpp
@@ -1,8 +1,81 @@
 #include <iostream>
 #include <Windows.h>
+#include <string.h>
 
+#define CLIENT_END_EVENT_NAME "CLIENT_END_EVENT"
+#define PIPE_NAME "\\\\.\\pipe\\server_pipe"
+#define CLIENT_PROCESS "client.exe"
 
-int main() {
-    std::cout << "Hello, i'm server!";
+#define LINE_LEN 512
+
+int main()
+{
+    HANDLE clientEndEvent = CreateEvent(NULL, FALSE, FALSE, CLIENT_END_EVENT_NAME);
+    if (clientEndEvent == NULL)
+    {
+        std::cout << "Creating client end event failed." << '\n';
+        return GetLastError();
+    }
+
+    HANDLE pipe = CreateNamedPipe(PIPE_NAME, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_WAIT, 1, 0, 0, INFINITE, NULL);
+    if (pipe == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Creating pipe failed." << '\n';
+        CloseHandle(clientEndEvent);
+        return GetLastError();
+    }
+
+    STARTUPINFO si;  
+    PROCESS_INFORMATION piApp; 
+    ZeroMemory(&si, sizeof(STARTUPINFO));  
+    si.cb = sizeof(STARTUPINFO); 
+    if (!CreateProcess(CLIENT_PROCESS, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp))
+    {
+        std::cout << "Error were occurred while creating child process.";
+        CloseHandle(clientEndEvent);
+        CloseHandle(pipe);
+        return 0;
+    }
+
+    std::cout << "Server is waiting client connection...\n";
+    if (!ConnectNamedPipe(pipe, NULL))
+    {
+        std::cout << "Client doesn't connected to server!";
+        CloseHandle(piApp.hThread);  
+        CloseHandle(piApp.hProcess); 
+        CloseHandle(clientEndEvent);
+        CloseHandle(pipe);
+        return GetLastError();
+    }
+
+    char str[LINE_LEN];
+    DWORD dwBytesWrite;
+    while (true)
+    {
+        std::cout << "To finish server process enter q. Enter string: ";
+        std::cin.getline(str, LINE_LEN);
+        if (!WriteFile(pipe, str, sizeof(str), &dwBytesWrite, NULL))
+        {
+            std::cout << "Error were occurred while writing to pipe.";
+            CloseHandle(piApp.hThread);  
+            CloseHandle(piApp.hProcess); 
+            CloseHandle(clientEndEvent);
+            CloseHandle(pipe);
+            return GetLastError();
+        }
+
+        std::cout << "Send string to client process...\n";
+        WaitForSingleObject(clientEndEvent, INFINITE);
+        if (strcmp(str, "q") == 0)
+        {
+            break;
+        }
+    }
+    std::cout << "Server process finish work.";
+    WaitForSingleObject(piApp.hProcess, INFINITE);
+    CloseHandle(piApp.hThread);  
+    CloseHandle(piApp.hProcess); 
+    CloseHandle(clientEndEvent);
+    CloseHandle(pipe);
     return 0;
 }
