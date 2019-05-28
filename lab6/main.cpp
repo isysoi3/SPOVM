cda446f07097db601ccacb314efa82cbdcecf5fc
Ilya Sysoi <14ilya12322@gmail.com>
2019-05-28 14:35:58 +0300

    refactored code

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -2,85 +2,90 @@
 #include <windowsx.h>
 #include <iostream>
 
-// находит место и возвращает дескриптор
+// находит место и возвращает указатель
 void* myMalloc(int size) {
-	void* pLocal;
-	UINT uMaxFreeMem;
-	
-	pLocal = memoryAllocation(size);
+	void* pLocal = memoryAllocation(size);
 
-    if(pLocal == NULL) {
+    if (pLocal == nullptr) {
 		// попытка дефрагментации памяти и ее повторного выделения
-		uMaxFreeMem = LocalCompact(size);
+		UINT uMaxFreeMem = LocalCompact(size);
 		std::cout << "Trying to defragmentate memory" << std::endl;
 		pLocal = memoryAllocation(size);
-		if (pLocal == NULL) {
+		if (pLocal == nullptr) {
 			std::cout << "Not enough memory" << std::endl;
 			return nullptr;
-		}
-	}
-	return pLocal;
+		} else {
+            return pLocal;
+        }
+	} else {
+	    return pLocal;
+    }
 }
 
+//выделение памяти
 void* memoryAllocation(int size) {
-	HLOCAL hmemLocal;
-	void*  pLocal;
+    //Выделяет указанное количество байтов из кучи.
+	HLOCAL hmemLocal = LocalAlloc(GHND, size + sizeof(HLOCAL));
 
-	// получение дескриптора выделяемой памяти
-	hmemLocal = LocalAlloc(GHND, size + sizeof(HLOCAL));
-
-	if(hmemLocal != NULL) {
-		// Если буфер получен, фиксируем его в памяти
-		pLocal = LocalLock(hmemLocal);
-		if(pLocal != NULL) {
+	if (hmemLocal != NULL) {
+        //Блокирует объект локальной памяти и возвращает указатель на первый байт блока памяти объекта.
+		void* pLocal = LocalLock(hmemLocal);
+		if (pLocal != NULL) {
 			// Запись дескриптора в начало выделенной памяти
 			pLocal = hmemLocal;
-			// установка начала памяти на следующий после дескриптора байт
-			pLocal = (char*)pLocal +sizeof(HLOCAL);  
+			// Установка начала памяти на следующий после дескриптора байт
+			pLocal = (char*)pLocal + sizeof(HLOCAL); //(мне не нрав что чар!!!ЛИЗА)
 			return pLocal;
 		} else {
-			std::cout<<std::endl<<"Error in locking block";
+			std::cout << "Error in locking block" << std::endl;
+            std::cout << GetLastError() << std::endl;
+            return nullptr;
 		}
 	} else {
-	    std::cout<<std::endl<<"Not enough memory";
+	    std::cout << "Not enough memory" << std::endl;
+        std::cout << GetLastError() << std::endl;
+        return nullptr;
 	}
-	return nullptr;
 }
 
-void  myFree(void* ptr) {
-	HLOCAL hmemLocal;
-	// получение дескриптора памяти
-	hmemLocal=(char*)ptr - sizeof(HLOCAL);
+void myFree(void* ptr) {
+    // получение дескриптора памяти
+	HLOCAL hmemLocal = (char*)ptr - sizeof(HLOCAL); //(мне не нрав что чар!!!ЛИЗА)
+
 	// разлок hmemLocal
 	LocalUnlock(hmemLocal);
+
 	// возвращает NULL при успешном освобождении памяти
-	if(LocalFree(hmemLocal) != NULL) {
-		std::cout <<std::endl << "Error in making memory free";
+	if (LocalFree(hmemLocal) != NULL) {
+		std::cout << "Error in making memory free" << std::endl ; 
+        std::cout << GetLastError() << std::endl;
 	} else {
 		std::cout << std::endl << "Memory is free";
+        ptr = nullptr;
 	}
-	ptr=nullptr;
 }
  
 void* myRealloc(void* ptr, int newSize) {
-	
+    // получение дескриптора памяти
 	HLOCAL hmemLoDiscard = (char*)ptr - sizeof(HLOCAL);
 
-	hmemLoDiscard = LocalReAlloc(hmemLoDiscard,newSize,NULL);
+    //Измене размера указанного объекта локальной памяти. Размер может увеличиваться или уменьшаться.
+	hmemLoDiscard = LocalReAlloc(hmemLoDiscard, newSize, NULL);
 
 	if(hmemLoDiscard == NULL) {
-		std::cout<<std::endl<<"Error in locking block";
+		std::cout << "Error in realloc block" << std::endl;
+        std::cout << GetLastError() <<std::endl;
 		return nullptr;
 	}
+
 	void* startMem = (char*)ptr - sizeof(HLOCAL);
 	startMem = LocalLock(hmemLoDiscard);
 	if(startMem == NULL) {
-		std::cout<<std::endl<<"Error in locking block";
-		std::cout<<std::endl<<GetLastError();
-
+		std::cout << "Error in locking block" << std::endl;
+		std::cout << GetLastError() <<std::endl;
 	}
 	startMem = hmemLoDiscard;
-	return (char*)startMem+sizeof(HLOCAL);
+	return (char*)startMem + sizeof(HLOCAL);
 }
 
 
